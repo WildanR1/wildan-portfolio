1b7888981be9541bff81dbd9476f9326e43b251c
Wildan Rachmadoni <wildanrachmadoni@gmail.com>
2024-02-09 23:18:16 +0700

    add vercel analytic and speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -52,6 +54,8 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <body className={inter.className}>
         <Providers>{children}</Providers>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,4 @@
-"use client";
 import React from "react";
-import Footer from "@/components/Footer";
-import NavbarLandingpage from "@/components/NavbarLandingpage";
-import Navbar from "@/components/Navbar";
-import About from "@/components/About";
-import Portfolio from "@/components/Portfolio";
-import Certificate from "@/components/Certificate";
-import Contact from "@/components/Contact";
 import Landingpage from "@/views/Landingpage";
 
 export default function Home() {
